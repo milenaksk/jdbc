094b86cbb15b51623cb0201998db1f96622823b1
milenaksk <cristinemilena32@gmail.com>
2023-02-06 15:22:37 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,11 +7,6 @@ import java.sql.Connection;
 public class Program {
     public static void main(String[] args) {
         Connection conn = DB.getConnection(); //chamando o método para se conectar ao banco
-
-
-
-
-
         DB.closeConnection();
     }
 }
